95e22406608331effb120eb959c5abb1b10492eb
jicool91 <jicool912@gmail.com>
2017-06-25 19:07:30 +0300

    charCodeAt + String.fromCharCode

--- es6-test.js
+++ es6-test.js
@@ -1,7 +1,13 @@
-function test(a, b){
-  return a + b;
-}
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-var test = function(a, b){
-  return a + b;
-}
+const res = arr.map(el => el * el);
+
+/**
+ * Это моя ебаная функция, которая возвращает сумма агрументов.
+ *
+ * @param {number} a
+ * @param {number} b
+ */
+const sum = (a, b) => a + b;
+
+const blabla = sum(10, 7);
